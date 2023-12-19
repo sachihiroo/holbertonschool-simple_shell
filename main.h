eaa79df807bfa84d9cec3917ebc7e70839d24b56
sachihiroo <saif3antar@gmail.com>
2023-12-19 12:50:10 +0100

    Header file

---
+++ main.h
@@ -0,0 +1,24 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+
+char *read_line(void);
+char **tokenizer(char *line);
+
+char *_strdup(char *str);
+int _strcmp(char *s1, char *s2);
+int _strlen(char *s);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+
+
+#endif
