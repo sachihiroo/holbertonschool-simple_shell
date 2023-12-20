8f6981f5d9231c810226bf01b15a118328fde287
amina <amina.1111@yahoo.fr>
2023-12-20 13:21:24 +0100

    main.h

--- main.h
+++ main.h
@@ -19,6 +19,7 @@ int _strcmp(char *s1, char *s2);
 int _strlen(char *s);
 char *_strcat(char *dest, char *src);
 char *_strcpy(char *dest, char *src);
-
+int _exec(char **cmd, char **argv);
+void _free(char **str);
 
 #endif
