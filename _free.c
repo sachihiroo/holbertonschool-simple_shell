9982e923efd01d8ab963b833f08559a8c6ff9ca4
amina <amina.1111@yahoo.fr>
2023-12-21 22:43:44 +0100

    free the memory

--- _free.c
+++ _free.c
@@ -1,19 +1,25 @@
 #include "main.h"
 
+/**
+ * _free - a function designed to free the memory associated
+ * with a two-dimensional array of strings
+ * @str: two-dimensional array of strings.
+ */
+
 void _free(char **str)
 
 {
-    int i;
+	int i;
 
-    if (str == NULL)
-    {
-        return;
-    }
-    for (i = 0; str[i]; i++)
-    {
-        free(str[i]);
-        str[i] = NULL;
-    }
-    free(str);
-    str = NULL;
+	if (str == NULL)
+	{
+		return;
+	}
+	for (i = 0; str[i]; i++)
+	{
+		free(str[i]);
+		str[i] = NULL;
+	}
+	free(str);
+	str = NULL;
 }
