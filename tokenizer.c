3426f1b9e45154d5bf1582c32b51290ee8f457c4
amina <amina.1111@yahoo.fr>
2023-12-21 13:01:06 +0100

    Parse the user's input into individual words

---
+++ tokenizer.c
@@ -0,0 +1,52 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+char **tokenizer(char *line)
+{
+	char *token;
+	char *tmp = NULL;
+	char delim[] = " \t\n";
+	char **cmd = NULL;
+	int i = 0, j = 0;
+
+	if (line == NULL)
+		return (NULL);
+
+	tmp = strdup(line);
+	token = strtok(tmp, delim);
+
+	if (token == NULL)
+	{
+		free(line), line = NULL;
+		free(tmp), tmp = NULL;
+		return (NULL);
+	}
+
+	while (token)
+	{
+		i++;
+		token = strtok(NULL, delim);
+	}
+	free(tmp), tmp = NULL;
+
+	cmd = malloc(sizeof(char *) * (i + 1));
+	if (cmd == NULL)
+	{
+		free(line);
+		return (NULL);
+	}
+
+	token = strtok(line, delim);
+	while (token)
+	{
+		cmd[j] = strdup(token);
+		token = strtok(NULL, delim);
+		j++;
+	}
+	free(line), line = NULL;
+	cmd[j] = NULL;
+	return (cmd);
+}
