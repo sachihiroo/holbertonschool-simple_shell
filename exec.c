64dd910fcb9f377ad600df96b79474651a899e58
amina <amina.1111@yahoo.fr>
2023-12-21 13:02:23 +0100

    execution file

--- exec.c
+++ exec.c
@@ -3,24 +3,26 @@
 int _exec(char **cmd, char **argv)
 
 {
-    pid_t child;
-    int stat;
+	char *full_cmd;
+	pid_t child;
+	int stat;
 
-    child = fork();
-    if (child == 0)
-    {
-        if (execve(cmd[0], cmd, environ) == -1)
-        {
-            perror(argv[0]);
-            _free(cmd);
-            exit(127);
-        }
-    }
-
-    else
-    {
-        waitpid(child, &stat, 0);
-        _free(cmd);
-    }
-    return (WEXITSTATUS(stat));
+	full_cmd = get_path(cmd[0]);
+	child = fork();
+	if (child == 0)
+	{
+		if (execve(full_cmd, cmd, environ) == -1)
+		{
+			perror(argv[0]);
+			_free(cmd);
+		}
+	}
+	else
+	{
+		waitpid(child, &stat, 0);
+		if (strcmp(cmd[0], full_cmd) != 0)
+			free(full_cmd), full_cmd = NULL;
+		_free(cmd);
+	}
+	return (WEXITSTATUS(stat));
 }
